de9d3a048981fdb39dba1beaa367604699a481ae
Joppe-VH <joppe.vanhoof@gmail.com>
2025-01-22 01:40:39 +0100

    add mongoose connection

--- src/server.ts
+++ src/server.ts
@@ -5,8 +5,10 @@ import express from "express";
 import { notFound } from "./controllers/notFoundController";
 import testRoutes from "./routes/exampleRoutes";
 import { helloMiddleware } from "./middleware/exampleMiddleware";
+import mongoose from "mongoose";
 
 // Variables
+const { DB_HOST, DB_PORT, DB_USER, DB_PASS, DB_DATABASE } = process.env;
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -17,6 +19,18 @@ app.use(express.json());
 // Routes
 app.use("/api", helloMiddleware, testRoutes);
 app.all("*", notFound);
+
+// Database connection
+try {
+  await mongoose.connect(`mongodb://${DB_HOST}:${DB_PORT}/${DB_DATABASE}`, {
+    user: DB_USER,
+    pass: DB_PASS,
+  });
+  console.log("Database connection OK");
+} catch (err) {
+  console.error(err);
+  process.exit(1);
+}
 
 // Server Listening
 app.listen(PORT, () => {
